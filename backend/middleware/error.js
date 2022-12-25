6d574aac7bf7bba29b1baa206c13bab1e953c02c
alokprinc <alokprince8699@gmail.com>
2022-12-26 02:23:25 +0530

    Error handling done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,23 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // Mongoose duplicate error
+  if (err.code === 11000) {
+    const message = `${Object.keys(err.keyValue)} Already exists`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT error
+  if (err.name === "jsonWebTokenError") {
+    const message = `Json Web Token is Invalid, Try again`;
+    err = new ErrorHandler(message, 400);
+  }
+  // Expired JWT error
+  if (err.name === "TokenExpiredError") {
+    const message = `Json Web Token Expired, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
